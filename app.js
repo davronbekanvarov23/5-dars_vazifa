45547dee50b7865ca71368b71fc5a89ed95cb470
davronbekanvarov23 <davronbekanvarov23@gmail.com>
2024-02-22 16:12:03 +0500

    completed

---
+++ app.js
@@ -0,0 +1,71 @@
+// 1-MASALA
+// function sum(x, y) {
+//   console.log(x + y);
+// }
+// sum(4, 5);
+
+// 2 - MASALA;
+
+// function tosekunds(minutes) {
+//   console.log(minutes * 60);
+// }
+
+// tosekunds(5);
+
+// 3-MASALA
+
+// function nextinteger(int) {
+//   console.log(int+1);
+// }
+// nextinteger(-3);
+
+// 4-MASALA
+
+// function UchburchakYuzi(asos, balandlik) {
+//   console.log((asos * balandlik) / 2);
+// }
+// UchburchakYuzi(3, 2);
+
+// 5-MASALA
+
+// function agetodays(age) {
+//   console.log(age * 365);
+// }
+
+// agetodays(65);
+
+// 6-MASALA
+
+// function kub(son) {
+//   console.log(son ** 3);
+// }
+// kub(5);
+
+// 7-MASALA
+
+// function firstElement(a, b, c) {
+//   console.log(a);
+// }
+// firstElement(1, 2, 3);
+
+// 8-MASALA
+
+// function power(x, y) {
+//   console.log(x * y);
+// }
+// power(5, 89);
+
+// 9-MASALA
+
+// function hourToSeconds(hour) {
+//   console.log(hour * 60 ** 2);
+// }
+
+// hourToSeconds(24);
+
+// 10-MASALA
+
+// function uchunchiTomon(tomon1, tomon2) {
+//   console.log(tomon1 + tomon2 - 1);
+// }
+// uchunchiTomon(8, 10);
